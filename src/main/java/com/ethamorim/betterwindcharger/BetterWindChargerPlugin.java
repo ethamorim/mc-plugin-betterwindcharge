e4bda2d6f1529d18dc81220e33101cc1a92a51f8
ethamorim <ethamorim@gmail.com>
2024-07-22 08:20:39 -0300

    feat: slow wind charger down

--- src/main/java/com/ethamorim/betterwindcharger/BetterWindChargerPlugin.java
+++ src/main/java/com/ethamorim/betterwindcharger/BetterWindChargerPlugin.java
@@ -1,17 +1,35 @@
 package com.ethamorim.betterwindcharger;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Fireball;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
-public final class BetterWindChargerPlugin extends JavaPlugin {
+public final class BetterWindChargerPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        System.out.println("Wooohoo! My Plugin has started!!");
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        var windCharger = new ItemStack(Material.WIND_CHARGE, 64);
+        e.getPlayer().getInventory().addItem(windCharger);
+    }
+
+    @EventHandler
+    public void onProjectileLaunch(ProjectileLaunchEvent e) {
+        if (e.getEntity() instanceof Fireball windAsFireball) {
+            var velocity = windAsFireball.getVelocity();
+            windAsFireball.setVelocity(new Vector(velocity.getX() / 16, velocity.getY() / 16, velocity.getZ() / 16));
+        }
+
     }
 }
